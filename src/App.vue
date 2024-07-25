36f20697cbe07518a3f0a2429aae63d0e1507f82
eric <howcrazys@gmail.com>
2024-07-25 16:24:57 +0800

    feat: 初步 datatable vee-vaidate 資料連接

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,58 @@
 <script setup>
-import {ref} from 'vue'
-const products = ref([
+import zod from 'zod'
+const initData = [
   { code: 'ABC123', name: 'Product 1' },
   { code: 'DEF456', name: 'Product 2' },
   { code: 'GHI789', name: 'Product 3' },
   { code: 'JKL012', name: 'Product 4' },
   { code: 'MNO345', name: 'Product 5' }
+]
+
+const columns = ref([
+  { field: 'code', header: 'Code' },
+  { field: 'name', header: 'Name' }
 ])
+
+const vTempData = ref([])
+
+const itemSchema = zod.object({
+  code: zod.string(),
+  name: zod.string()
+})
+
+useForm({
+  validationSchema: toTypedSchema(zod.array(itemSchema)),
+  initialValues: vTempData
+})
+
+const { replace, fields } = useFieldArray('vTempData')
+
+const converFields = computed(() => {
+  return fields.value?.map((item) => ({ ...item.value, vKey: item.key })) || []
+})
+onMounted(() => {
+  replace([...initData])
+})
 </script>
 
 <template>
   <header></header>
-
   <main>
     <div class="container">
       <div class="row">
         <div class="col-12">
+          <h2 class="text-center pt-3 mb-1">PrimeVue DataTable + Vee Validate (Zod) :</h2>
+          <h3 class="text-center mb-3">array validation (simple CRUD)</h3>
           <div class="text-end">
             <button class="btn btn-primary">新增</button>
           </div>
-          <DataTable :value="products" tableStyle="min-width: 50rem">
-            <Column field="code" header="Code"></Column>
-            <Column field="name" header="Name"></Column>
+          <DataTable :value="converFields" tableStyle="min-width: 50rem" datakey="vKey">
+            <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header">
+              <template #body="slotProps">
+                <!-- {{ slotProps.data[`${col.field}`] }} -->
+                <VeeInput :value="slotProps.data[`${col.field}`]"></VeeInput>
+              </template>
+            </Column>
           </DataTable>
         </div>
       </div>
---
+++ src/components/VeeInput.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  inputMethod: {
+    type: Function,
+    default: null
+  },
+  value: {
+    type: [String, Number],
+    default: ''
+  }
+})
+</script>
+<template>
+  <input type="text" class="form-control" :value="props.value" />
+</template>
